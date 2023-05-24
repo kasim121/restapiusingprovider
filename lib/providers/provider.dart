4d6f5eaf8c88e91371ce476f2bdd4d3d98a98823
Mohd Kasim <kasimplecoder@email.com>
2023-05-24 09:30:27 +0530

    changes done

--- lib/providers/provider.dart
+++ lib/providers/provider.dart
@@ -8,26 +8,23 @@ import '../models/model.dart';
 class MyProvider extends ChangeNotifier {
   static const apiEndpoint =
       'https://fmtest.dishco.com/shawmanservices/api/SPAGetServiceLineDetailsPTR/FunGetServiceLineDetails?SourceApp=WLA&PackageName=8X8syJfkvzrxI5fLHCbSqQIYViVLh2R016thr3Fw7L8%3D&SPAId=3';
-
+  static const apiLogin ='https://reqres.in/api/login';
+      
   bool isLoading = true;
   String error = '';
   ServiceLineDetailsModel? serviceLineDetailsModel;
-  
 
-
-  
   getDataFromAPI() async {
     try {
-      Response response = await http.get(Uri.parse(apiEndpoint),headers:{
-      'AndroidPhone': 'EV6FTlgBhOalM+qjJpr2OZpAEpPkYJHC5I1aOWyeLevwSIpuzyKEAg==',
-
-     
-
-  
-    });
+      Response response = await http.get(Uri.parse(apiEndpoint), headers: {
+        'AndroidPhone':
+            'EV6FTlgBhOalM+qjJpr2OZpAEpPkYJHC5I1aOWyeLevwSIpuzyKEAg==',
+      });
       if (response.statusCode == 200) {
-        serviceLineDetailsModel = serviceLineDetailsModelFromJson(response.body);
-      debugPrint("printing.. ${serviceLineDetailsModel!.serviceLineDetails!.first.serviceLineDescription}");
+        serviceLineDetailsModel =
+            serviceLineDetailsModelFromJson(response.body);
+        debugPrint(
+            "printing.. ${serviceLineDetailsModel!.serviceLineDetails!.first.serviceLineDescription}");
       } else {
         error = response.statusCode.toString();
       }
@@ -36,11 +33,28 @@ class MyProvider extends ChangeNotifier {
     }
     isLoading = false;
     notifyListeners();
-    
   }
 
-  
-
+  postLogin(String email, String name) async {
+    try {
+      Response response = await http.post(Uri.parse(apiLogin), body: {
+        'email': email,
+        'name': name,
+      });
+      if (response.statusCode == 200) {
+        serviceLineDetailsModel =
+            serviceLineDetailsModelFromJson(response.body);
+        debugPrint(
+            "printing.. ${serviceLineDetailsModel!.serviceLineDetails!.first.serviceLineDescription}");
+      } else {
+        error = response.statusCode.toString();
+      }
+    } catch (e) {
+      error = e.toString();
+    }
+    isLoading = false;
+    notifyListeners();
+  }
 
   //
 }
